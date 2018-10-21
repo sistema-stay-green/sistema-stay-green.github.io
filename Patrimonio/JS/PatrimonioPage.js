3fdebb970dcb371c6de8ade419b2500fb03441a2
Sacardoso <savioshippuden13@gmail.com>
2018-10-20 23:08:20 -0300

    HTML, CSS e JS da página
    base da página HTML pronta, com CSS faltando as estilizações do Pedro e JS de troca do body da página

---
+++ Patrimonio/JS/PatrimonioPage.js
@@ -0,0 +1,31 @@
+function showComprarPage(){
+    document.querySelector("#header").style.display = "block";
+    document.querySelector("#comprar").style.display = "block";
+    document.querySelector("#entrada").style.display = "none";
+    document.querySelector("#saida").style.display = "none";
+    document.querySelector("#tabela").style.display = "none";
+}
+
+function showEntradaPage(){
+    document.querySelector("#header").style.display = "block";
+    document.querySelector("#comprar").style.display = "none";
+    document.querySelector("#entrada").style.display = "block";
+    document.querySelector("#saida").style.display = "none";
+    document.querySelector("#tabela").style.display = "none";
+}
+
+function showSaidaPage(){
+    document.querySelector("#header").style.display = "block";
+    document.querySelector("#comprar").style.display = "none";
+    document.querySelector("#entrada").style.display = "none";
+    document.querySelector("#saida").style.display = "block";
+    document.querySelector("#tabela").style.display = "none";
+}
+
+function showPatrimonioPage(){
+    document.querySelector("#header").style.display = "block";
+    document.querySelector("#comprar").style.display = "none";
+    document.querySelector("#entrada").style.display = "none";
+    document.querySelector("#saida").style.display = "none";
+    document.querySelector("#tabela").style.display = "block";
+}

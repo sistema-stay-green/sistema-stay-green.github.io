c467d02d086eab98d2ae01aad900e40f980fba38
Sacardoso <savioshippuden13@gmail.com>
2018-10-23 00:24:21 -0300

    HTML quase pronto
    HTML basicamente pronto faltando concertar uns erros no CSS

---
+++ Patrimonio/JS/PatrimonioEncapsulate.js
@@ -0,0 +1,14 @@
+function encapsulaDados(){
+    let patrimonio = new Patrimonio(5);
+    patrimonio.nome = document.querySelector('#nome_patrimonio').value;
+    patrimonio.tipo = document.querySelector('#tipo_patrimonio').value;
+    patrimonio.descricao = document.querySelector('#descricao_patrimonio').value;
+    patrimonio.status = 'EM_POSSE';
+    patrimonio.indiceDepreciacao = document.querySelector('#indice_depreciacao').value;
+    patrimonio.valorCompra = document.querySelector('#valor_compra').value;
+    patrimonio.valorAtual = document.querySelector('#valor_compra').value;
+    let data = document.querySelector('#data_compra').value.split('-');
+    patrimonio.dataCompra = new Date(data[0], data[1], data[2]);;
+
+    patrimonio.printToConsole();
+}
--- Patrimonio/JS/PatrimonioPage.js
+++ Patrimonio/JS/PatrimonioPage.js
@@ -1,31 +1,21 @@
 function showComprarPage(){
     document.querySelector("#header").style.display = "block";
     document.querySelector("#comprar").style.display = "block";
-    document.querySelector("#entrada").style.display = "none";
-    document.querySelector("#saida").style.display = "none";
     document.querySelector("#tabela").style.display = "none";
 }
 
 function showEntradaPage(){
-    document.querySelector("#header").style.display = "block";
-    document.querySelector("#comprar").style.display = "none";
-    document.querySelector("#entrada").style.display = "block";
-    document.querySelector("#saida").style.display = "none";
-    document.querySelector("#tabela").style.display = "none";
+    document.querySelector("#entrada").classList.toggle("aparece");
+    document.querySelector("#saida").classList.remove("aparece");
 }
 
 function showSaidaPage(){
-    document.querySelector("#header").style.display = "block";
-    document.querySelector("#comprar").style.display = "none";
-    document.querySelector("#entrada").style.display = "none";
-    document.querySelector("#saida").style.display = "block";
-    document.querySelector("#tabela").style.display = "none";
+    document.querySelector("#saida").classList.toggle("aparece");
+    document.querySelector("#entrada").classList.remove("aparece");
 }
 
 function showPatrimonioPage(){
     document.querySelector("#header").style.display = "block";
     document.querySelector("#comprar").style.display = "none";
-    document.querySelector("#entrada").style.display = "none";
-    document.querySelector("#saida").style.display = "none";
     document.querySelector("#tabela").style.display = "block";
 }

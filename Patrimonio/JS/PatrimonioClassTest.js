fb5a6fb1e06e7d7a7d3edbb961f924c30ef320c4
Sacardoso <savioshippuden13@gmail.com>
2018-10-23 00:24:21 -0300

    HTML quase pronto
    HTML basicamente pronto faltando concertar uns erros no CSS

--- Patrimonio/JS/PatrimonioClassTest.js
+++ Patrimonio/JS/PatrimonioClassTest.js
@@ -1,23 +1,13 @@
-function recebeDadosCompra(){
-    let patrimonio = new Patrimonio(5);
-    patrimonio.nome = document.querySelector('#nome_patrimonio').value;
-    patrimonio.tipo = document.querySelector('#tipo_patrimonio').value;
-    patrimonio.descricao = document.querySelector('#descricao_patrimonio').value;
-    patrimonio.status = "EM POSSE";
-    patrimonio.indiceDepreciacao = document.querySelector('#indice_depreciacao').value;
-    patrimonio.valorCompra = document.querySelector('#valor_compra').value;
-    patrimonio.valorAtual = document.querySelector('#valor_compra').value;
-    patrimonio.dataCompra = document.querySelector('#data_compra').value;
-    patrimonio.dataBaixa = null;
-    patrimonio.dataSaida = null;
+let patrimonio = new Patrimonio(5);
+patrimonio.nome = "Adriana";
+patrimonio.tipo = "Trator"
+patrimonio.descricao = "Meio chata mas a gente aguenta";
+patrimonio.status = "VENDIDO"; // Valores predefinidos na classe Patrimonio
+patrimonio.indiceDepreciacao = "1.5";
+patrimonio.valorCompra = "5"; // Ao mandar uma String, o Setter tentará converte-la para um número.
+patrimonio.valorAtual = 3;
+patrimonio.dataCompra = new Date(1950, 12, 30, 23, 59, 59, 100); // Data + Hora, min, seg, ms.
+patrimonio.dataBaixa = new Date(2020, 11, 28); // Data
+patrimonio.dataSaida = new Date(); // Data atual
 
-    patrimonio.printToConsole();
-}
-
-function recebeDadosEntrada(){}
-
-function recebeDadosSaida(){}
-
-function atualizaTabela(){
-  
-}
+patrimonio.printToConsole();

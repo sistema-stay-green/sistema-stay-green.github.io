eb1c2a538aab422b3831a04cacafc21c39a7e059
Mei Fagundes <MeiFagundes@outlook.com>
2018-10-27 18:11:35 -0300

    Create insertPatrimonioIntoTable to append a Patrimonio into the page

--- Patrimonio/JS/PatrimonioClassTest.js
+++ Patrimonio/JS/PatrimonioClassTest.js
@@ -1,13 +1,16 @@
 let patrimonio = new Patrimonio(5);
 patrimonio.nome = "Adriana";
 patrimonio.tipo = "Trator"
-patrimonio.descricao = "Meio chata mas a gente aguenta";
+patrimonio.finalidade = "Meio chata mas a gente aguenta";
 patrimonio.status = "VENDIDO"; // Valores predefinidos na classe Patrimonio
 patrimonio.indiceDepreciacao = "1.5";
-patrimonio.valorCompra = "5"; // Ao mandar uma String, o Setter tentará converte-la para um número.
-patrimonio.valorAtual = 3;
+patrimonio.valorCompra = "5000"; // Ao mandar uma String, o Setter tentará converte-la para um número.
+patrimonio.valorAtual = 3800;
 patrimonio.dataCompra = new Date(1950, 12, 30, 23, 59, 59, 100); // Data + Hora, min, seg, ms.
-patrimonio.dataBaixa = new Date(2020, 11, 28); // Data
 patrimonio.dataSaida = new Date(); // Data atual
+patrimonio.dataRetorno = new Date(); // Data atual
+patrimonio.dataBaixa = new Date(2020, 11, 28); // Data
 
 patrimonio.printToConsole();
+
+insertPatrimonioIntoTable(patrimonio);
---
+++ Patrimonio/JS/PatrimonioManipulationService.js
@@ -0,0 +1,87 @@
+
+const patrimonioTable = document.querySelector("#patrimonioTable tbody");
+
+/**
+ * Indenta um objeto Patrimonio na tabela principal.
+ * @param {Patrimonio} patrimonio 
+ * @author Mei
+ */
+function insertPatrimonioIntoTable(patrimonio = new Patrimonio()){
+
+    let tr = document.createElement("tr");
+    let td;
+    let button;
+
+    td = document.createElement("td");
+    td.innerHTML = patrimonio.id;
+    tr.appendChild(td);
+
+    td = document.createElement("td");
+    td.innerHTML = patrimonio.nome;
+    tr.appendChild(td);
+
+    td = document.createElement("td");
+    td.innerHTML = patrimonio.tipo;
+    tr.appendChild(td);
+
+    td = document.createElement("td");
+    td.innerHTML = patrimonio.finalidade;
+    tr.appendChild(td);
+
+    td = document.createElement("td");
+    td.innerHTML = patrimonio.status;
+    tr.appendChild(td);
+
+    td = document.createElement("td");
+    td.innerHTML = patrimonio.indiceDepreciacao;
+    tr.appendChild(td);
+
+    td = document.createElement("td");
+    td.innerHTML = patrimonio.valorCompra;
+    tr.appendChild(td);
+
+    td = document.createElement("td");
+    td.innerHTML = patrimonio.valorAtual;
+    tr.appendChild(td);
+
+    td = document.createElement("td");
+    td.innerHTML = patrimonio.dataCompra.toISOString().slice(0,10).replace("/-/g","");
+    tr.appendChild(td);
+
+    td = document.createElement("td");
+    td.innerHTML = patrimonio.dataSaida.toISOString().slice(0,10).replace("/-/g","");
+    tr.appendChild(td);
+
+    td = document.createElement("td");
+    td.innerHTML = patrimonio.dataRetorno.toISOString().slice(0,10).replace("/-/g","");
+    tr.appendChild(td);
+
+    td = document.createElement("td");
+    td.innerHTML = patrimonio.dataBaixa.toISOString().slice(0,10).replace("/-/g","");
+    tr.appendChild(td);
+
+    // Adicionando os botões com id's variáveis. Ex: 'edit-7'
+    td = document.createElement("td"); 
+
+    button = document.createElement("button");
+    button.id = "edit-" + patrimonio.id;
+    button.innerHTML = "Editar";
+    td.appendChild(button);
+
+    button = document.createElement("button");
+    button.id = "delete-" + patrimonio.id;
+    button.innerHTML = "Deletar";
+    td.appendChild(button);
+
+    tr.appendChild(td);
+
+    patrimonioTable.appendChild(tr);
+
+}
+
+function addPatrimonio(patrimonio = new Patrimonio()){
+
+    // Send to servlet
+    // Receive back from Servlet
+    // add to page with 'insertPatrimonioIntoPage' function
+}

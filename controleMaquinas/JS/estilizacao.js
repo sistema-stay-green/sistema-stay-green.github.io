4fb85475d7d5b99fe69ad83f6a3571d95fbe3a5a
Gabriel7g <gabriel.gomesgalo@gmail.com>
2018-10-31 11:02:54 -0300

    Atualização Maquinas

--- controleMaquinas/JS/estilizacao.js
+++ controleMaquinas/JS/estilizacao.js
@@ -1,19 +1,31 @@
-/**
- * Classe objeto para a encapsulação de dados do Patrimonio.
- * @author Italo
- */
-document.querySelector("#BotãoCadastro").addEventListener("click", Cadastrar);
-document.querySelector("#BotãoVoltar").addEventListener("click", Voltar);
-document.querySelector("#BotãoAção").addEventListener("click", cadastroSemBD);
-var chamaSaida = document.querySelectorAll(".BotãoSaida");
+document.querySelector("button[name='BotãoCadastro']").addEventListener("click", Cadastrar);
+document.querySelector("button[name='BotãoVoltar']").addEventListener("click", Voltar);
+document.querySelector("button[name='BotãoAção']").addEventListener("click", cadastroSemBD);
+let chamaSaida = document.querySelectorAll(".BotãoSaida");
 for (var i = 0; i < chamaSaida.length; i++) {
   chamaSaida[i].addEventListener("click", saida);
 }
 setTimeout(VisãoBotão, 100);
 
+let inputs = document.querySelectorAll(".inputs");
+
+for (let input of inputs){
+  input.addEventListener("input", function(e){
+    let alvoEl = e.currentTarget;
+    if(alvoEl.value === ""){
+      let btnCadastroEl = document.querySelector("button[name='BotãoAção']");
+      btnCadastroEl.disable = true;
+      btnCadastroEl.classList.add("botaoDesab");
+    }
+  })
+}
+
+
+
+
 function Voltar(){
   document.querySelector(".maquinas").style.display = "inline-block";
-  document.querySelector(".opções").style.display = "block";
+  //document.querySelector(".opções").style.display = "block";
   document.querySelector(".valores").style.display = "none";
   document.querySelector(".inputs").value = "";
 
@@ -24,21 +36,20 @@ function Cadastrar(){
   //adiciona uma maquina no BD
 
     document.querySelector(".valores").style.display = "block";
-    document.querySelector(".opções").style.display = "none";
-    document.querySelector(".maquinas").style.display = "none";
+    //document.querySelector(".opções").style.display = "none";
+    //document.querySelector(".maquinas").style.display = "none";
 
 }
 //função temporaria sem BD
 function cadastroSemBD(){
   //teste sem o BD
-  let nome = document.querySelector("#nome").value,
-  finalidade = document.querySelector("#finalidade").value,
-  valor =  document.querySelector("#valor").value,
-  data = document.querySelector("#data").value,
-  depreciação = document.querySelector("#depreciação").value,
+  let nome = document.querySelector("input[name='nome']").value,
+  finalidade = document.querySelector("input[name='finalidade']").value,
+  valor =  document.querySelector("input[name='valor']").value,
+  data = document.querySelector("input[name='data']").value,
+  depreciação = document.querySelector("input[name='depreciação']").value,
   BotãoSaida = "<button type=\"button\" class=\"BotãoSaida\">Saida</button>",
-  maquinas = document.querySelector(".tabela");
-
+  maquinas = document.querySelector("#tabela");
   let vetor = [nome,finalidade,valor,depreciação,data];
   let string = "";
 
@@ -57,17 +68,20 @@ function cadastroSemBD(){
   document.querySelector(".maquinas").style.display = "inline-block";
   document.querySelector(".opções").style.display = "block";
   document.querySelector(".valores").style.display = "none";
-  document.querySelector(".inputs").value = "";
+
+  var valores = document.querySelectorAll(".inputs");
+  for (var i = 0; i < valores.length; i++) {
+    valores[i].value = "";
+  }
   var botões = document.querySelectorAll(".BotãoSaida");
   for (var i = 0; i < botões.length; i++) {
     botões[i].addEventListener("click", saida);
   }
-
 }
 
 function saida(){
-  var opcao = document.querySelector("#filtro_saida");
-  var elemento = event.target;
+  let opcao = document.querySelector("#filtro_saida");
+  let elemento = event.target;
   elemento = elemento.parentNode;
   nodes = elemento.parentNode.children;
   console.log(elemento.parentNode);
@@ -103,7 +117,7 @@ function saida(){
           }
         });
     }
-    var botão = document.querySelectorAll(".BotãoEnviar");
+    let botão = document.querySelectorAll(".BotãoEnviar");
     for (var i = 0; i < botão.length; i++) {
       botão[i].addEventListener("click", function(){
         AlteraStatus(elemento,opcao);
@@ -128,7 +142,8 @@ window.onclick = function(event) {
 
 function AlteraStatus(elemento,opcao){
   console.log(elemento);
-  elemento = elemento.parentNode;
+  elemento = elemento.parentElement;
+  console.log(elemento);
   nodes = elemento.children;
   var vetor = [];
   for (var i = 0; i < nodes.length; i++) {
@@ -160,7 +175,12 @@ function AlteraStatus(elemento,opcao){
     botoes[i].addEventListener("click", saida);
   }
   VisãoBotão();
+  var valores = document.querySelectorAll(".inputs");
+  for (var i = 0; i < valores.length; i++) {
+    valores[i].value = "";
+  }
 }
+
 
 function VisãoBotão() {
   var botões = document.querySelectorAll(".BotãoSaida");
@@ -168,7 +188,7 @@ function VisãoBotão() {
     var pesquisa = botões[i].parentNode;
     pesquisa = pesquisa.parentNode;
     var nodes = pesquisa.children;
-    // console.log(nodes);
+    console.log(nodes);
     if(nodes[6].innerHTML != "EM_POSSE"){
       botões[i].className = "botaoDesab";
     }
@@ -176,5 +196,5 @@ function VisãoBotão() {
       botões[i].className = "BotãoSaida";
     }
   }
-  // console.log("teste");
+  console.log("teste");
 }

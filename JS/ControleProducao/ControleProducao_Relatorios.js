fd3ffb5d9dd02e83201a5e6e1f64c19fedc29063
Thiago <thileps7@gmail.com>
2018-11-05 10:47:36 -0200

    Js do modal

---
+++ JS/ControleProducao/ControleProducao_Relatorios.js
@@ -0,0 +1,13 @@
+var btnCriaRelatorioEl = document.getElementById("btnCriaRelatorio");
+var btnFechaModalEl = document.getElementById("btnFechaModal");
+
+btnCriaRelatorioEl.addEventListener('click', mostraDivModal);
+btnFechaModalEl.addEventListener('click',  mostraDivModal);
+
+function mostraDivModal() {
+  var divModalEl = document.querySelector("#divModal");
+  var divMascaraEl = document.querySelector("#divMascara");
+  divModalEl.classList.toggle("esconde");
+  divModalEl.classList.toggle("posDivModal");
+  divMascaraEl.classList.toggle("ocultar")
+}

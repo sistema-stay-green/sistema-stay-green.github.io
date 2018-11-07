f54444a86a5412d8398d15050fd3506d14533737
Bárbara Martins <martins.barbara04@gmail.com>
2018-11-07 12:07:05 -0200

    Alterações nas páginas de login e perfil

--- JS/Gerencia/perfil.js
+++ JS/Gerencia/perfil.js
@@ -85,3 +85,19 @@ function checaEmail() {
   }
 
 }
+
+// AJAX
+let btnConfimarCadastroEl = document.querySelector("#cadastro > button:first-of-type");
+
+btnConfimarCadastroEl.addEventListener("click", function() {
+
+  let nomeUsuario = document.querySelector("#cadastro input[name='nomeUsuario']").value,
+      cnpjUsuario = document.querySelector("#cadastro input[name='cnpjUsuario']").value,
+      saldoUsuario = document.querySelector("#cadastro input[name='saldoUsuario']").value,
+      emailUsuario = document.querySelector("#cadastro input[name='emailUsuario']").value,
+      senhaUsuario = document.querySelector("#cadastro input[name='senhaUsuario']").value;
+
+  Request.get("http://localhost:8080/StayGreen/UpdateUsuarioServlet?nome=" + nomeUsuario + "&cnpj=" + cnpjUsuario + "&saldo=" + saldoUsuario + "&login=" + emailUsuario + "&senha=" + senhaUsuario);
+  
+
+});

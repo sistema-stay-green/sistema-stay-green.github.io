7ab94e43b140e1cd131a95c48af6d94e02eafc6e
Quorthon13 <diegodemetrio79@gmail.com>
2018-11-14 11:43:34 -0200

    Metodo Relatório por Periodo

--- JS/ControleProducao/ControleProducao_RelatorioMercadoriasPeriodo.js
+++ JS/ControleProducao/ControleProducao_RelatorioMercadoriasPeriodo.js
@@ -1,13 +1,15 @@
 //@autor Alberto
 
 var btnRelatorioH = document.getElementById('btnRelatorioHistorico');
+var conteudoRelatorio = document.getElementById('modalRelatorioHistorico');
 
 btnRelatorioH.addEventListener('click', criaRelatorioH());
 
 function criaRelatorioH(){
   var periodo = document.getElementById('selPeriodo').value;
-  Request.get("http:localhost:8080/StayGreen/ControleProducaoServlet?operacao=Relatorio1&JSON=periodo").then(function(resultado){
-    console.log(resultado);
+  Request.get("http:localhost:8080/StayGreen/ControleProducaoServlet?operacao=Relatorio1&JSON="+periodo).then(function(resultado){
+    var objeto = JSON.parse(resposta);
+    conteudoRelatorio.innerHTML = "nome:"+ objeto.nome;
   })
   .catch(function(erro){console.log(erro);});
 

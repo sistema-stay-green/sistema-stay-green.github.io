3c9acea54c5db6192b4d90a9bc3e71df42398415
Acreisjr <acreisjunio@gmail.com>
2018-11-15 14:25:12 -0200

    Pequenas mudanças HTML e JS

--- JS/ControleProducao/ControleProducao_RelatoriosMercadoriasPeriodo.js
+++ JS/ControleProducao/ControleProducao_RelatoriosMercadoriasPeriodo.js
@@ -1,13 +1,16 @@
 //@autor Alberto
 
 var btnRelatorioH = document.getElementById('btnRelatorioHistorico');
+var conteudoRelatorioH = document.getElementById('conteudoRelatorioHistorico');
 
-btnRelatorioH.addEventListener('click', criaRelatorioH());
+btnRelatorioH.addEventListener('click', criaRelatorioH);
 
 function criaRelatorioH(){
   var periodo = document.getElementById('selPeriodo').value;
+  console.log(periodo);
   Request.get("http:localhost:8080/StayGreen/ControleProducaoServlet?operacao=Relatorio1&id=" + periodo).then(function(resultado){
     console.log(resultado);
+    conteudoRelatorioH.innerHTML = resultado.nomeProduto;
   })
   .catch(function(erro){console.log(erro);});
 

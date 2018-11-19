24987bcb78dff8f1db3a2c0b6dde579013d71e67
Rubens Souza <rubensvictor.m.s@gmail.com>
2018-11-19 12:39:24 -0200

    Verifica se o usuario esta logado no index

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,6 @@
-let cardAgenda = document.querySelector("#agenda");
-let cardProducao = document.querySelector("#producao");
-let cardPatrimonio = document.querySelector("#patrimonio");
-let cardMaquinas = document.querySelector("#maquinas");
-let cardVendas = document.querySelector("#vendas");
-
 function mudaUrl(url){
   window.location = url;
 }
-
-cardAgenda.addEventListener('click', function(){ mudaUrl("agendaEletronica.html"); });
-cardVendas.addEventListener('click', function(){ mudaUrl("paginaVendasProdutor.html"); });
-cardPatrimonio.addEventListener('click', function(){ mudaUrl("Patrimonio.html"); });
-cardMaquinas.addEventListener('click', function(){ mudaUrl("controleMaquinas.html"); });
-cardProducao.addEventListener('click', function(){ mudaUrl("controleProducao.html"); });
 
 //transition
 document.querySelector('#comecar').addEventListener('click', function (e) {
@@ -22,3 +10,50 @@ document.querySelector('#comecar').addEventListener('click', function (e) {
   });
 });
 
+const usuarioLogado = true;
+
+//let dadosUsuario = JSON.parse(localStorage.getItem('usuario'));
+//if(dadosUsuario === null){ naoEstaLogado } else{ logado }
+
+if(usuarioLogado){ //não logado
+
+  let cards = document.querySelectorAll("#main > article");
+
+  for (let cardEl of cards) {
+
+    if (cardEl.id === "perfil") {
+
+      cardEl.addEventListener('click', function(){ mudaUrl("login.html"); });
+      cardEl.querySelector('p').innerHTML = "Caso seja um adiminstrador, faça login no sistema para ter acesso as outras funcionalidades";
+      cardEl.querySelector('h3').innerHTML = "Login e Cadastro";
+
+    }
+    else if (cardEl.id === "vendas") {
+
+      cardEl.addEventListener('click', function(){ mudaUrl("paginaVendas.html"); });
+      cardEl.querySelector('p').innerHTML = "Caso seja um comprador, acesse essa página para pedir nosso produtos";
+      cardEl.querySelector('h3').innerHTML = "Site de vendas";
+
+    }
+    else
+      cardEl.classList.add("oculta");
+
+  }
+
+}
+else{ //logado
+  let cardAgenda = document.querySelector("#agenda");
+      cardProducao = document.querySelector("#producao"),
+      cardPatrimonio = document.querySelector("#patrimonio"),
+      cardMaquinas = document.querySelector("#maquinas"),
+      cardVendas = document.querySelector("#vendas"),
+      cardPefil = document.querySelectorAll("#perfil")
+
+
+  cardAgenda.addEventListener('click', function(){ mudaUrl("agendaEletronica.html"); });
+  cardVendas.addEventListener('click', function(){ mudaUrl("paginaVendasProdutor.html"); });
+  cardPatrimonio.addEventListener('click', function(){ mudaUrl("Patrimonio.html"); });
+  cardMaquinas.addEventListener('click', function(){ mudaUrl("controleMaquinas.html"); });
+  cardProducao.addEventListener('click', function(){ mudaUrl("controleProducao.html"); });
+  cardPefil.addEventListener('click', function(){ mudaUrl("perfil.html"); });
+}

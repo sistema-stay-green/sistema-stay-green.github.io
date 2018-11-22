66448b5b0b22bbf2378cdbb8b87cc9ca14649d10
Beto <acreisjunio@gmail.com>
2018-11-22 09:33:18 -0200

    Relatórios praticamente completos
    O relatório 2 está sujeito à algumas mudanças

--- JS/ControleProducao/ControleProducao_RelatoriosMercadoriasPeriodo.js
+++ JS/ControleProducao/ControleProducao_RelatoriosMercadoriasPeriodo.js
@@ -1,5 +1,5 @@
 /** @autor Alberto
-    js responsável pelos relatórios HTML**/
+js responsável pelos relatórios HTML**/
 
 //DOM
 var btnRelatorioH = document.getElementById('btnRelatorioHistorico');
@@ -16,20 +16,31 @@ var tabelaInsumo = document.getElementById('tabelaInsumo');
 var tituloProduto = document.getElementById('tituloProduto');
 var tituloInsumo = document.getElementById('tituloInsumo');
 var btnRelatorioProducao = document.getElementById('btnRelatorioProducao');
+var textoRelProducao = document.getElementById('textoRelProducao');
 
 btnRelatorioProducao.addEventListener('click', criaRelatorioP);
 btnRelatorioH.addEventListener('click', criaRelatorioH);
 
 function criaRelatorioP() {
   Request.get("http:localhost:8080/StayGreen/ControleProducaoServlet?operacao=relatorio2&id=" + "teste")
-  .then(function(resultado){console.log(resultado);}).catch(function(erro){console.log(erro);});
+  .then(function(resultado){
+    console.log(resultado);
+    if (resultado == 0) {
+      textoRelProducao.innerHTML = "Não foi possivel calcular a Produção Semanal";
+      textoRelProducao.style.color = "red";
+    }else{
+      textoRelProducao.innerHTML = "Produção Semanal: " + resultado + "$";
+    }
+  })
+  .catch(function(erro){console.log(erro);});
+
 }
 
 
 
 /** @autor Alberto
-    Descrição: função que apartir de um periodo faz uma requisição
-    para o servlet que retorna um ArrayList com as transações feitas**/
+Descrição: função que apartir de um periodo faz uma requisição
+para o servlet que retorna um ArrayList com as transações feitas**/
 function criaRelatorioH(){
   var periodo = document.getElementById('selPeriodo').value;
   var contLeite = 0;
@@ -42,7 +53,7 @@ function criaRelatorioH(){
   .then(function(resultado){
 
     var contador = 0;
-
+    //testa se há resultados disponiveis do request
     if(resultado == null){
       avisoRelatorio.innerHTML == "" ? avisoRelatorio.innerHTML = "Não foram encontradas transações nesse período": avisoRelatorio.innerHTML="Não foram " +
       "encontradas transações nesse período";
@@ -55,36 +66,33 @@ function criaRelatorioH(){
         if(resultado[contador].tipoTransacao == "PRODUTO"){
           switch (resultado[contador].idItemTransacao) {
             case 1:
-              contLeite++;
-              var linha = tabelaProdutoLeite.insertRow(contLeite + 1);
-              insereProdutoTabela(resultado, contador, linha);
-              break;
+            contLeite++;
+            var linha = tabelaProdutoLeite.insertRow(contLeite + 1);
+            insereProdutoTabela(resultado, contador, linha);
+            break;
             case 2:
-              contCafeB++;
-              var linha = tabelaProdutoCafeB.insertRow(contCafeB + 1);
-              insereProdutoTabela(resultado, contador, linha);
-              break;
+            contCafeB++;
+            var linha = tabelaProdutoCafeB.insertRow(contCafeB + 1);
+            insereProdutoTabela(resultado, contador, linha);
+            break;
             case 3:
-                contCafeR++;
-                var linha = tabelaProdutoCafeR.insertRow(contCafeR + 1);
-                insereProdutoTabela(resultado, contador, linha);
-                break;
+            contCafeR++;
+            var linha = tabelaProdutoCafeR.insertRow(contCafeR + 1);
+            insereProdutoTabela(resultado, contador, linha);
+            break;
             case 4:
-              contCafeA++;
-              var linha = tabelaProdutoCafeA.insertRow(contCafeA + 1);
-              insereProdutoTabela(resultado, contador, linha);
-              break;
+            contCafeA++;
+            var linha = tabelaProdutoCafeA.insertRow(contCafeA + 1);
+            insereProdutoTabela(resultado, contador, linha);
+            break;
             default:
 
           }
           //teste para saber se a transação é de Insumo
         }else if(resultado[contador].tipoTransacao == "INSUMO"){
-             contInsumos++;
-              var linha = tabelaInsumo.insertRow(contInsumos + 1);
-              insereInsumoTabela(resultado, contador, linha);
-
-
-
+          contInsumos++;
+          var linha = tabelaInsumo.insertRow(contInsumos + 1);
+          insereInsumoTabela(resultado, contador, linha);
         }
 
         contador++;
@@ -95,17 +103,17 @@ function criaRelatorioH(){
   .catch(function(erro){console.log(erro);});
 }
 /** @autor Alberto
-    Descrição: função que a partir do resultado e do contador formata a data da transação**/
+Descrição: função que a partir do resultado e do contador formata a data da transação**/
 function formataData(resultado, contador){
   var mesIncrementado = resultado[contador].dataTransacao.month + 1;
   var dataFormatada = resultado[contador].dataTransacao.dayOfMonth + "/" + mesIncrementado + "/" +
-   resultado[contador].dataTransacao.year;
+  resultado[contador].dataTransacao.year;
   return dataFormatada;
 }
 
 /** @autor Alberto
-    Descrição: função que ajusta o Nome do Produto recuperado do BD, afim de tornar
-    mais agradável visualmente para o usuário **/
+Descrição: função que ajusta o Nome do Produto recuperado do BD, afim de tornar
+mais agradável visualmente para o usuário **/
 function ajustaNomeProduto(resposta){
   var vetorTemp = resposta.nomeProduto.split("_");
   if (vetorTemp["length"] > 1) {
@@ -114,63 +122,62 @@ function ajustaNomeProduto(resposta){
   return vetorTemp;
 }
 
-
+/** @autor Alberto
+Descrição: Insere os produtos em suas respectivas tabelas, preenchendo-as
+com os devidos valores  **/
 function insereProdutoTabela(resultado, contador, linha){
+  var valTransacao = resultado[contador].valorTransacao;
+  //para cada uma das colunas da tabela insere o respectivo valor
   for(let i = 1; i <= 4; i++){
-
     var celula = linha.insertCell(i-1);
     switch(i){
       case 1:
+      valTransacao > 0 ? celula.innerHTML = "Venda" : celula.innerHTML = "Compra";
+      break;
+      case 2:
       celula.innerHTML = formataData(resultado, contador);
 
       break;
-      case 2:
+      case 3:
       celula.innerHTML = resultado[contador].quantTransacao;
       break;
-      case 3:
-      celula.innerHTML = resultado[contador].valorTransacao+"$";
+      case 4:
+      valTransacao > 0 ? celula.innerHTML = valTransacao.toFixed(2) + "$" : celula.innerHTML = (valTransacao * -1).toFixed(2) +"$" ;
       break;
     }
   }
 
 }
+/** @autor Alberto
+Descrição: Insere os insumos em uma tabela, preenchendo-as
+com os devidos valores  **/
 function insereInsumoTabela(resultado, contador, linha){
   console.log(resultado[contador]);
   Request.get("http:localhost:8080/StayGreen/ControleProducaoServlet?operacao=buscar&id="+resultado[contador].idItemTransacao+"&tipo=insumo")
   .then(function(resposta){
-      var nome = resposta.nomeInsumo;
-      console.log(nome);
-      for(let i = 1; i <= 4; i++){
-        var celula = linha.insertCell(i-1);
-        switch(i){
-          case 1:
+    var valTransacao = resultado[contador].valorTransacao;
+    console.log(valTransacao);
+    var nome = resposta.nomeInsumo;
+    for(let i = 1; i <= 5; i++){
+      var celula = linha.insertCell(i-1);
+      switch(i){
+        case 1:
+          valTransacao > 0 ? celula.innerHTML = "Venda" : celula.innerHTML = "Compra";
+          break;
+        case 2:
           celula.innerHTML = nome;
           break;
-          case 2:
+        case 3:
           celula.innerHTML = formataData(resultado, contador);
-
           break;
-          case 3:
+        case 4:
           celula.innerHTML = resultado[contador].quantTransacao;
           break;
-          case 4:
-          celula.innerHTML = resultado[contador].valorTransacao+"$";
+        case 5:
+          valTransacao > 0 ? celula.innerHTML = valTransacao.toFixed(2) + "$" : celula.innerHTML = (valTransacao * -1).toFixed(2) +"$" ;
           break;
-        }
       }
+    }
   })
   .catch(function(erro){console.log(erro);});
 }
-
-
-// function requestProduto(resultado, contador, idItem){
-//   Request.get("http:localhost:8080/StayGreen/ControleProducaoServlet?operacao=buscar&id="+
-//   idItem+"&tipo=produto").then(function(resposta){
-//     var textoRel = ajustaNomeProduto(resposta) + ": " + resultado[contador].valorTransacao+" R$ "+
-//       formataData(resultado, contador);
-//     encontraTipoProduto(idItem).innerHTML = textoRel;
-//
-//
-//   })
-//   .catch(function(erro){console.log(erro);});
-// }
